8328e12662fb94f01b1034ec133bbba632acbedd
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-01 13:42:34 +0300

    start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='yeyeyeye'
+export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost/pitch'
+export MAIL_USERNAME='stephenndele093@gmail.com'
+export MAIL_PASSWORD='Newbeggining2020!'
+
+
+python3.8 manage.py runserver
