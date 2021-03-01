ba9e5e6c3934868def346f1a7a172541358f4340
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-01 23:45:52 +0300

    database configure

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='yeyeyeye'
-export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost:5432/pitch'
+export DATABASE_URL='postgresql+psycopg2://moringa:Access@127.0.0.1:5432/pitch'
 export MAIL_USERNAME='stephenndele093@gmail.com'
 export MAIL_PASSWORD='Newbeggining2020!'
 
