c40ea4b3716fa1a6bcd2cd0438ed70467e03775c
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-01 13:54:49 +0300

    add user test for password

--- tests/user_test.py
+++ tests/user_test.py
@@ -14,4 +14,4 @@ class UserTest(unittest.TestCase):
             self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('qwerty'))
+        self.assertTrue(self.new_user.verify_password('12345'))
