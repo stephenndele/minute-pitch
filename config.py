42fa823ca0afc617e78eb74014c7e968177bb38f
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-02 09:25:16 +0300

    change runtime version

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
